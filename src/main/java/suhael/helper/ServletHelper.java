1dfcc5243defab61ea2c178dc43dd88198c4c941
suhael <suhael_akhtar@hotmaill.com>
2015-11-13 21:34:45 +0000

    add view helper

---
+++ src/main/java/suhael/helper/ServletHelper.java
@@ -0,0 +1,18 @@
+package suhael.helper;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ServletHelper {
+
+    public static void renderView(HttpServletRequest request,
+                                  HttpServletResponse response, String jspFileLocation)
+            throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = request
+                .getRequestDispatcher(jspFileLocation);
+        requestDispatcher.forward(request, response);
+    }
+}
