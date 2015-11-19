20dcba5171643713c727e1df619bc239d87eaf91
Suhael Akhtar <suhael.akhtar@maersk.com>
2015-11-19 09:25:42 +0000

    add cdi support

--- servletLib/src/main/java/com/suhael/javaEE/lib/TestServlet.java
+++ servletLib/src/main/java/com/suhael/javaEE/lib/TestServlet.java
@@ -1,5 +1,9 @@
 package com.suhael.javaEE.lib;
 
+import com.suhael.SayHello;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,14 +11,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@RequestScoped
 public class TestServlet extends HttpServlet {
+
+    @Inject
+    private SayHello sayHello;
 
     public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         doGet(req, res);
     }
 
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getOutputStream().print("it works!");
+        resp.getOutputStream().println(sayHello.getName());
+        resp.getOutputStream().println("it works!");
         resp.setStatus(200);
     }
 }
--- webProject/src/main/java/com/suhael/javaEE/servlet/PingServlet.java
+++ webProject/src/main/java/com/suhael/javaEE/servlet/PingServlet.java
@@ -2,20 +2,27 @@ package com.suhael.javaEE.servlet;
 
 import com.suhael.SayHello;
 
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@RequestScoped
 public class PingServlet extends HttpServlet {
+
+    @Inject
+    private SayHello sayHello;
+
 
     public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         doGet(req, res);
     }
 
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        SayHello sayHello = new SayHello();
+//        SayHello sayHello = new SayHello();
         resp.getOutputStream().println(sayHello.getName());
         resp.getOutputStream().println("pong");
         resp.setStatus(200);
